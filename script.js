6c60f29bef2ff8d186c07b0d049e04cc5611fc13
Kc-man6 <kcboytimo@gmail.com>
2025-07-05 20:26:58 +0100

    Responsive Furniture Website

--- script.js
+++ script.js
@@ -30,5 +30,13 @@ updateRange();
 
 
 
+// Hamburger menu functionality
 
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  hamburger.classList.toggle("active"); // this triggers
+});
 
